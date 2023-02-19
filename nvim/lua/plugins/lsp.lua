a6ada7a815b77e5536443a34406583c3111b341e
Kevin Viricel <kevin.viricel@gmail.com>
2023-02-19 15:02:35 +0100

    Remove flake8 from null-ls

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -15,10 +15,11 @@ return {
     "jose-elias-alvarez/null-ls.nvim",
     opts = function(_, opts)
       local nls = require("null-ls")
-      opts.sources = vim.list_extend(opts.sources, {
+      opts.sources = {
+        nls.builtins.formatting.stylua,
         nls.builtins.formatting.black,
         nls.builtins.formatting.isort,
-      }, 1, 2)
+      }
     end,
   },
 }
