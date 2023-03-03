d44017e2eb6d8404529532f09e4926df59486b6b
Kevin Viricel <kevin.viricel@gmail.com>
2023-03-03 12:30:31 +0100

    Bordered completion and documentation window

---
+++ nvim/lua/plugins/cmp.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function()
+      local cmp = require("cmp")
+
+      cmp.setup({
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        },
+      })
+    end,
+  },
+}
