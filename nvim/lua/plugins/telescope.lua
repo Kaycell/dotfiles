1adf62c6adf50f4380f11fc122e35a2fccd8b705
Kevin Viricel <kevin.viricel@gmail.com>
2023-11-30 11:46:37 -0500

    Update lazyvim

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,12 +1,6 @@
-local Util = require("lazyvim.util")
-
 return {
   {
     "nvim-telescope/telescope.nvim",
-    keys = {
-      { "<leader>/", Util.telescope("live_grep", { cwd = false }), desc = "Find in Files (cwd)" },
-      { "<leader><space>", Util.telescope("files", { cwd = false }), desc = "Find Files (cwd)" },
-    },
     opts = {
       defaults = {
         mappings = {
