345ad2718bf1d87bd89935a4f7ec2923b0b0aaa1
Kevin Viricel <kevin.viricel@proton.me>
2025-12-09 12:42:17 +0100

    System clipboard keymaps

--- dot_config/nvim/lua/keymaps.lua
+++ dot_config/nvim/lua/keymaps.lua
@@ -1,5 +1,26 @@
+local glb = vim.g
 local keymap = vim.keymap.set
 
-vim.g.mapleader = ' '
+glb.mapleader = ' '
 
 keymap('n', '<space>', '<Nop>')
+
+-- System clipboard
+keymap({ "n", "v" }, "<leader>y", '"+y', {
+    desc = "Yank into the system clipboard"
+})
+keymap({ "n", "v" }, "<leader>Y", '"+Y', {
+    desc = "Yank to EOL into the system clipboard"
+})
+keymap({ "n", "v" }, "<leader>d", '"+d', {
+    desc = "Cut into the system clipboard"
+})
+keymap({ "n", "v" }, "<leader>D", '"+D', {
+    desc = "Cut to EOL into the system clipboard"
+})
+keymap({ "n", "v" }, "<leader>p", '"+p', {
+    desc = "Paste the system clipboard after cursor"
+})
+keymap({ "n", "v" }, "<leader>P", '"+P', {
+    desc = "Paste the system clipboard before cursor"
+})
