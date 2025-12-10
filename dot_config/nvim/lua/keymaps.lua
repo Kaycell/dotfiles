584a07da9d1862c2b85822d0ab3da870ff33a328
Kevin Viricel <kevin.viricel@proton.me>
2025-12-10 11:55:41 +0100

    Better system clipboard keymap

--- dot_config/nvim/lua/keymaps.lua
+++ dot_config/nvim/lua/keymaps.lua
@@ -5,22 +5,27 @@ glb.mapleader = ' '
 
 keymap('n', '<space>', '<Nop>')
 
+-- Search
+keymap('n', '<Esc>', '<cmd>nohlsearch<CR>', {
+    desc = 'Clear highlights on search when pressing <Esc> in normal mode'
+})
+
 -- System clipboard
-keymap({ "n", "v" }, "<leader>y", '"+y', {
-    desc = "Yank into the system clipboard"
+keymap({ 'n', 'v' }, '<leader>y', '"+y', {
+    desc = 'Yank into the system clipboard'
 })
-keymap({ "n", "v" }, "<leader>Y", '"+Y', {
-    desc = "Yank to EOL into the system clipboard"
+keymap({ 'n', 'v' }, '<leader>Y', '"+y$', {
+    desc = 'Yank to EOL into the system clipboard'
 })
-keymap({ "n", "v" }, "<leader>d", '"+d', {
-    desc = "Cut into the system clipboard"
+keymap({ 'n', 'v' }, '<leader>d', '"+d', {
+    desc = 'Cut into the system clipboard'
 })
-keymap({ "n", "v" }, "<leader>D", '"+D', {
-    desc = "Cut to EOL into the system clipboard"
+keymap({ 'n', 'v' }, '<leader>D', '"+d$', {
+    desc = 'Cut to EOL into the system clipboard'
 })
-keymap({ "n", "v" }, "<leader>p", '"+p', {
-    desc = "Paste the system clipboard after cursor"
+keymap({ 'n', 'v' }, '<leader>p', '"+p', {
+    desc = 'Paste the system clipboard after cursor'
 })
-keymap({ "n", "v" }, "<leader>P", '"+P', {
-    desc = "Paste the system clipboard before cursor"
+keymap({ 'n', 'v' }, '<leader>P', '"+P', {
+    desc = 'Paste the system clipboard before cursor'
 })
